a6ac9f00df2fe20e0665653343979d29a2aaf6ad
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-04-21 01:19:53 +0300

    fetch Assignment completed

--- index.js
+++ index.js
@@ -17,3 +17,72 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+// below is playground
+
+
+//console.log(photos)
+
+// console.log("Photos Array")
+
+
+// setTimeout(() => console.log('one'), 3000)
+
+
+// fetch('https://jsonplaceholder.typicode.com/photos')
+// .then(res => console.log(res))
+
+
+// console.log('After')
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(res => {
+//     console.log(res)
+// })
+
+
+
+
+// function renderOnePhoto(photo){
+
+
+//     let card = document.createElement("li")
+//     card.className = 'card'
+//     card.innerHTML = `
+
+//         <img src="${photo.thumbnailUrl}">
+
+//         <div>
+//             <h4>${photo.title}</h4>
+
+//             <p>
+//             $<span class='photo-count'>${photo.id}</span> Counted
+//             </p>
+//         </div>
+
+
+//     ` 
+//     document.querySelector('#photo-list').appendChild(card)
+
+//     console.log(card)
+// }
+
+
+
+// function getPhotos(){
+//     fetch("https://jsonplaceholder.typicode.com/photos")
+//     .then(res => res.json())
+//     .then(photos => photo.forEach(photo => renderOnePhoto(photo)))
+//     console.log("before fetch returns")
+// }
+// initialize render
+// get data and render our photos to the Dom
+// function initialize(){
+//     photos.forEach(photo => renderOnePhoto(photo))
+//     getPhotos()
+//     console.log("after fetch get all")
+// }
+
+
+// initialize()
